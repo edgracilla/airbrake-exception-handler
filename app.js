e4b291860bc2cc811e59f8e8d28e5e9fe68ac217
edgracilla <ed.gracilla@gmail.com>
2017-03-28 17:24:40 +0800

    update

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const reekoh = require('reekoh')
-const _plugin = new reekoh.plugins.ExceptionLogger()
+const plugin = new reekoh.plugins.ExceptionLogger()
 const request = require('request')
 
 let url = null
 
-_plugin.on('exception', (error) => {
+plugin.on('exception', (error) => {
   request.post({
     url: url,
     json: true,
@@ -26,18 +26,18 @@ _plugin.on('exception', (error) => {
       }]
     }
   }, (error) => {
-    if (error) _plugin.logException(error)
+    if (error) plugin.logException(error)
   })
-  _plugin.log(JSON.stringify({
+  plugin.log(JSON.stringify({
     title: 'Exception sent to Airbrake',
     data: {message: error.message, stack: error.stack, name: error.name}
   }))
 })
 
-_plugin.once('ready', () => {
-  url = `https://airbrake.io/api/v3/projects/${_plugin.config.projectId}/notices?key=${_plugin.config.apiKey}`
-  _plugin.log('Air Brake Exception Logger has been initialized.')
-  _plugin.emit('init')
+plugin.once('ready', () => {
+  url = `https://airbrake.io/api/v3/projects/${plugin.config.projectId}/notices?key=${plugin.config.apiKey}`
+  plugin.log('Air Brake Exception Logger has been initialized.')
+  plugin.emit('init')
 })
 
-module.exports = _plugin
+module.exports = plugin
